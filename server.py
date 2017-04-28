7ba96b453f47c61a2a04782cdea811d6a2b637d1
Troy Routley <ptroutley@comcast.net>
2017-04-28 08:43:09 -0700

    began moving transmit into thread

--- server.py
+++ server.py
@@ -1,6 +1,50 @@
 import socket
 import select
 import tftp
+import argparse
+import sys
+
+def txfile(ip, port, filename, mode):
+    # send data packet to client
+    # wait for ack of the correct block
+    # send next data packet
+    # if incorrect ack received, do nothing
+    # if other type of packet received send error Packet
+    # wait for ack (block # should be last block received)
+    
+    print("ip: {} {} file: {} mode: {}".format(ip, port,
+          filename, mode))
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port+1))
+
+    f = open(filename,"rb")
+    opcode = 3
+    block = 1
+    done = False
+    #send data until packet size < 512
+    while (not done):
+        #read portion of file
+        data = f.read(512)
+        #if portion is smaller than 508, its the last block
+        if (sys.getsizeof(data) < 512):
+            done = True
+        #prepend opcode and block number
+        data = (opcode.to_bytes (2,byteorder='big')
+               + block.to_bytes (2,byteorder='big')
+               + data)
+        #send block
+        acknowledged = False
+        while (not acknowledged):
+            sock.sendto(data, (ip, port))
+            #wait for ack
+            ackData, addr = sock.recvfrom(516)
+            ackPack = tftp.Packet(ackData)
+            if (ackPack.opcode == 4 and ackPack.block == block):
+                #ack received
+                block += 1
+                acknowledged = True
+            #else resend
 
 # Simple TFTP server
 ####################
